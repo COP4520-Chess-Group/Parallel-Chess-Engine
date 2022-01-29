a3a1bc823293a09aca343ff3b13cdca62fce04e7
Nicholas Chitty <nicholas.chitty@gmail.com>
2022-01-29 18:04:09 -0500

    Initial Commit

---
+++ src/main/java/edu/ucf/cop4520/App.java
@@ -0,0 +1,9 @@
+package edu.ucf.cop4520;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/test/java/edu/ucf/cop4520/AppTest.java
@@ -0,0 +1,20 @@
+package edu.ucf.cop4520;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
