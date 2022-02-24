eaf03faf339a30b760986dde3ea33e0204a65896
Nicholas Chitty <nicholas.chitty@gmail.com>
2022-02-24 18:21:05 -0500

    Add evaluation object

--- src/main/java/edu/ucf/cop4520/game/evaluation/Evaluation.java
+++ src/main/java/edu/ucf/cop4520/game/evaluation/Evaluation.java
@@ -1,2 +1,22 @@
-package edu.ucf.cop4520.game.evaluation;public class Evaluation {
+package edu.ucf.cop4520.game.evaluation;
+
+import edu.ucf.cop4520.game.Board;
+import edu.ucf.cop4520.game.Move;
+
+public class Evaluation {
+
+    private double evaluation;
+    private Move move;
+    private Board board;
+
+    public Evaluation(Board board, Move move) {
+        this.move = move;
+        this.board = board.copyOf().move(move);
+        this.evaluation = this.evaluate();
+    }
+
+    private double evaluate() {
+        return evaluation;
+    }
+
 }
